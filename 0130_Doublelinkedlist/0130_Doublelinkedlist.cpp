9f0ae64c8784d290748e8ab228a41cb1d788fd91
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-05-05 10:26:22 +0700

    buat bidang sebelumnya dari node saat ini mengarah ke node baru

--- 0130_Doublelinkedlist/0130_Doublelinkedlist.cpp
+++ 0130_Doublelinkedlist/0130_Doublelinkedlist.cpp
@@ -46,5 +46,13 @@ void addNode()
             previous = current;
             current = current->next;
         }
+
+        newNode->next = current;
+        newNode->prev = previous;
+
+        if (current != NULL)
+        {
+            current->prev = newNode;
+        }
     }
 }
